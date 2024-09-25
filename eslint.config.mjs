aec05871a5a7efa144613e982ce147e3b65482fd
tarynblakemiller <yourfriendtaryn@gmail.com>
2024-09-25 04:30:17 -0400

    ci: lint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,52 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tseslintParser from "@typescript-eslint/parser";
+import reactPlugin from "eslint-plugin-react";
+import jestPlugin from "eslint-plugin-jest";
 
 export default [
+  js.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+        React: "readonly",
+      },
+      parser: tseslintParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      react: reactPlugin,
+      jest: jestPlugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...jestPlugin.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off", // Turn off requirement to import React in JSX files
+    },
     settings: {
       react: {
         version: "detect",
       },
     },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.test.{js,jsx,ts,tsx}"],
+    rules: {
+      // Disable no-undef for test files
+      "no-undef": "off",
+    },
+  },
 ];
